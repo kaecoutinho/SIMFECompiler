91d4d370f1c0132af0fe534f89bd3a75c083a18e
Kaê Angeli Coutinho <kae_coutinho@hotmail.com>
2015-11-09 19:32:21 -0200

    Update SIMFECompilerDefinitions.h

--- Source Code/SIMFECompilerDefinitions.h
+++ Source Code/SIMFECompilerDefinitions.h
@@ -1,6 +1,6 @@
 // SIMFECompilerDefinitions.h
 // SIMFECompiler
-// Created by Kaê Angeli Coutinho and Ricardo Oliete Ogata
+// Created by Kaê Angeli Coutinho
 // MIT license
 
 // Included dependencies
